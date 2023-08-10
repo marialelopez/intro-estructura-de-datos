a51625aa458860b10ea5b6a37aa785c740ad4829
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-08-09 21:59:23 -0500

    estructuras

---
+++ intro.js
@@ -0,0 +1,21 @@
+let variable = 3
+let string= "Alejandra"
+let x="3"
+let boolean = "3"+ 5
+
+let array = [2,"Alejandra",false, 'P', 3.5,"hola"]
+
+let objeto = {
+    numero:1,
+    nombre: "Alejandra",
+    boolena: false
+}
+
+
+console.log (objeto.nombre)
+
+console.log(variable)
+console.log(string)
+console.log(x)
+console.log(boolean)
+console.log(array[0])
